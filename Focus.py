47c5d4818e7b02bc2fb1d5bfade975521f3cefb5
kian <kn.behjati@gmail.com>
2023-03-02 18:45:44 +0330

    countdown timer in main programm and <not responding> fix + git ignore file

--- Focus.py
+++ Focus.py
@@ -5,15 +5,15 @@ import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter.ttk import *
+import threading
 
 '''
 
 @WinchesterAge
 
-this is first version of this app 
-i made it to keep me focused while i'm reading my leasons 
-OR coding other programms
 
+i made this app to keep me focused while i'm reading my leasons 
+OR coding other programms
 
 '''
 class focus:
@@ -24,6 +24,16 @@ class focus:
         
 
     def process_kill(self):
+
+        if " " in self.task:
+            
+            clear_task = ""
+
+            for char in self.task:
+
+                if char != " ":
+                    clear_task += char
+            self.task = clear_task
         
         passed_time = 0   #sec
 
@@ -54,10 +64,46 @@ class focus:
 
 if __name__ == "__main__":
 
+    is_running = False
+
     def clicked():
-        root.lower()
+
+        
+
+        def count():
+            Min = time_b.get()
+            while Min > 0:
+                l.config(text=f"{Min} minutes left",foreground="Red")
+                time.sleep(60.0)
+                Min -= 1
+            
+            l.config(foreground="Green")
+            
+            time.sleep(1)
+
+            root.destroy()
+
+            exit()
+            
+
+            
+                
+        
+        
         f = focus(task=process_name.get(), dur=time_b.get())
-        f.process_kill()
+        t1 = threading.Thread(target=f.process_kill).start()
+        l = ttk.Label(text="")
+        l.pack()
+        
+        t2 = threading.Thread(target=count).start()
+        
+        
+     
+        
+        
+
+        
+        
         
     root = tk.Tk()
     root.geometry('400x200')
@@ -68,14 +114,16 @@ if __name__ == "__main__":
     process_name = tk.StringVar()
     time_b = tk.IntVar()
 
-    lable = ttk.Label(text="Process Name").pack()
+    lable = ttk.Label(text="Programm Name").pack()
     process_name_box = ttk.Entry(root, textvariable=process_name).pack()
     
     ttk.Label(text="Time(min)").pack()
 
     time_box = ttk.Entry(root,textvariable=time_b).pack()
 
-    button = ttk.Button(root,text="Run",command=clicked,padding=10).pack(expand=True)
+    button = ttk.Button(root,text="Run",command=clicked,padding=10)
+    button.pack(expand=True)
+    
     
     root.mainloop()
     
